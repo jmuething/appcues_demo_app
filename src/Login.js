b0ad8cd100f3d93b3d012acb518690bd64117301
Kennedy Bryant <85645927+kennedybryant@users.noreply.github.com>
2026-02-12 11:33:04 -0500

    Adds US/EU account switching functionality

--- src/Login.js
+++ src/Login.js
@@ -1,5 +1,6 @@
 import React from "react";
 import AppcuesIdentifier from "./appcuesComponents/Identifier";
+import ScriptSwitcher from "./appcuesComponents/ScriptSwitcher";
 
 /* 
   feel free to add/remove any HTML you would like. Just remember that all content
@@ -11,6 +12,7 @@ const Login = () => {
     <div className="app-content">
       <h1>Login your user here</h1>
       <AppcuesIdentifier/>
+      <ScriptSwitcher/>
     </div>
   );
 };
